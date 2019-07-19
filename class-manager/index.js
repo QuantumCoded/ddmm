44323411ae69db2c3dddd7542367dc9af9a2acf1
QuantumCoded <mrbspony@gmail.com>
2019-07-19 09:28:07 -0600

    Removing unused classes from class manager

--- class-manager/index.js
+++ class-manager/index.js
@@ -1,8 +1,6 @@
 // Export classes as properties of the class manager
 module.exports.Client          = require('./classes/client');
-module.exports.Command         = require('./classes/command');
 module.exports.Guild           = require('./classes/guild');
 module.exports.Message         = require('./classes/message');
-module.exports.MessageTemplate = require('./classes/template');
 module.exports.Settings        = require('./classes/settings');
 module.exports.WebhookMessage  = require('./classes/webhook-message');
