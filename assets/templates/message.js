4b5e9fb0fc26999866bd0e60e16f8c5ad5e58ce3
QuantumCoded <mrbspony@gmail.com>
2019-07-27 19:06:46 -0600

    Adding support for getting names from profile

--- assets/templates/message.js
+++ assets/templates/message.js
@@ -1,8 +1,16 @@
+const Users = require('../../users');
+
 module.exports = function(message) {
+  let name;
+
+  if (Users.userExists(message.author.id))  {
+    name = Users.getUser(message.author.id).getProperty('name');
+  } else name = message.author.username;
+
   return {
     content: message.content,
     options: {
-      username: message.author.username, // NTS: Change to grab nick name here
+      username: name,
       avatarURL: message.author.avatarURL
     }
   };
