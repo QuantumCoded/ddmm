25efb21757111e732291d77022537211767c9b9b
QuantumCoded <mrbspony@gmail.com>
2019-07-27 19:06:23 -0600

    Implementing users into message class

--- class-manager/classes/message.js
+++ class-manager/classes/message.js
@@ -1,23 +1,13 @@
 const Channel = require('./channel');
 const WebhookMessage = require('./webhook-message');
 const Assets = require('../../assets');
+const Users = require('../../users');
 
 class Message {
-  type;
-  client;
-  author;
-  channel;
-  content;
-
-  
   constructor(type, message, client) {
     client.log.debug('Constructing a message');
 
-    this.type = type;
-    this.client = client; // Set the client value to the client
-    this.author = message.author; // Set the author value to the author of the message
-    this.channel = message.channel; // Set the channel value to the channel of the message
-    this.content = message.content; // Set the content value to the content of the message
+    let user;
 
     // Handle the message by type
     client.log.debug(`Handling the message type ${type}`);
@@ -47,49 +37,35 @@ class Message {
         new WebhookMessage(message.channel, Assets.getTemplate('note', message.content.replace(note_operator, '')));
       break;
       
-      // If the message is an incoming dm message handle opening a channel
+      // If the message is an incoming dm message handle sending the message
       case 'incoming':
-      // NTS: Change this to use users
+        // If the user exists then send them a message otherwise create a channel
+        if (Users.userExists(message.author.id)) {
+          user = Users.getUser(message.author.id); // Get the user
+          let channel = client.channels.get(user.getProperty('channel')); // Get the user's channel
 
-      client.log.debug('Getting user map from settings');
-        client.log.debug('Getting the channel for the user from the user map');
-        let userMap = client.settings.getValue('user-map'); // Get the user map from the client's settings
-        let channel = client.channels.get(userMap[message.author.id]); // Get the channel for the user
-
-        // If a channel exists for that user, send a message to it, otherwise create a channel with an initial message
-        if (channel) {
-          // Send the message to the channel
           client.log.debug('Sending a message to the channel');
-          new WebhookMessage(channel, Assets.getTemplate('message', message));
+          new WebhookMessage(channel, Assets.getTemplate('message', message)); // Send a message to the user's channel
         } else {
-          // Create the dms guild channel
-          client.log.debug('No channel found, creating a new one with initial message');
-          new Channel(client, message.author.username,
-            {
-              type: 'text',
-              recipient: message.author,
-              initialMessage: Assets.getTemplate('message', message)
-            }
-          );
+          // Create a new channel for the user with an initial message
+          new Channel(client, message.author.username, {
+            type: 'text',
+            recipient: message.author,
+            initialMessage: Assets.getTemplate('message', message),
+          });
         }
       break;
-      
+
       // If the message is a relayable message in a valid channel
       case 'relayable':
-        //NTS: Change this to use users
-
-        client.log.debug('Getting channel map from settings');
-        client.log.debug('Getting the user for the channel from the channel map');
-        let channelMap = client.settings.getValue('channel-map'); // Get the channel map from the client's settings
-        let user = client.users.get(channelMap[message.channel.id]); // Get the user for the channel
+        user = Users.getChannelRecipient(message.channel.id); // Get the user for the channel
 
         // If the user exists send a message to that user
         if (user) {
           client.log.debug('Relaying the message to the user');
-          user.send(message.content, {files: Array.from(message.attachments.values()).map(a => a.url)}); // Send a message to the user
+          client.users.get(user.getProperty('id')).send(message.content, {files: Array.from(message.attachments.values()).map(a => a.url)}); // Send a message to the user
         } else {
           // NTS: This can also be caused by someone removing you as a friend (maybe if the dm channel is closed) without updating the maps
-          client.log.error(`The channel map has an invalid entry for ${message.channel.id}: ${userId}`);
         }
       break;
     }
